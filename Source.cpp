f6890fd6b9113f1b8599eb6850e066f9f53eb9b6
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-04-13 16:10:45 +0300

    Call by value and call by reference.

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
-#include "DateClass.h"
 
 using namespace std;
 
-void PrintGlobal(DateClass theObject)
+// call by value mechanism
+// function parameters are local variables
+// original variables are copied
+void f(int i)
 {
-	cout << "PrintGlobal - begin" << endl;
-	theObject.Print();
-	cout << "PrintGlobal - end" << endl;
+	cout << "f - begin " << i << endl;
+	i *= 2;
+	cout << "f - end " << i << endl;
+}
+
+// call by reference mechanism
+// original variables are not copied
+// can also be used to avoid expensive object copies for large objects containing several fields
+void g(int& i)
+{
+	cout << "g - begin " << i << endl;
+	i *= 2;
+	cout << "g - end " << i << endl;
+}
+
+void h(int* i)
+{
+	cout << "h - begin " << *i << endl;
+	(*i) *= 2;
+	cout << "h - end " << *i << endl;
 }
 
 int main()
 {
-	cout << "main - begin" << endl;
+	int w = 10;
+	cout << "main - begin " << w << endl;
+	f(w);
+	cout << "main - after f" << w << endl;
 
-	DateClass today(2021, 4, 7);
+	g(w);
+	cout << "main - after g" << w << endl;
 
-	PrintGlobal(today);
-
-	cout << "main - end" << endl;
+	cout << "w = " << w << "&w = " << &w << endl;
+	h(&w);
+	cout << "main - after h" << w << endl;
 
 	return 0;
 }
