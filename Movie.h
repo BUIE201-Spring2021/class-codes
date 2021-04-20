fa5ad2e35ba77534e53d32a1c81da1ec340f5fe7
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-04-20 15:15:29 +0300

    Created Movie and Viewer classes.

---
+++ Movie.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include "DateClass.h"
+
+using namespace std;
+
+class Movie
+{
+	string Name;
+	double Score;
+
+	DateClass ReleaseDate;
+};
+
--- Source.cpp
+++ Source.cpp
@@ -3,22 +3,8 @@
 
 using namespace std;
 
-void PrintGlobal(DateClass theObject)
-{
-	cout << "PrintGlobal - begin" << endl;
-	theObject.Print();
-	cout << "PrintGlobal - end" << endl;
-}
-
 int main()
 {
-	cout << "main - begin" << endl;
-
-	DateClass today(2021, 4, 7);
-
-	PrintGlobal(today);
-
-	cout << "main - end" << endl;
 
 	return 0;
 }
