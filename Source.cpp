558c7070556aa944e41f7d7ea2e4489588d95889
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-04-21 14:34:06 +0300

    Added new functions

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello, world!" << endl;
+	vector<string> Words{ "hello", "how", "are", "you", "did", "I", "say", "hello", "or", "do",
+		"you", "want", "to", "say", "hello", "first" };
+
+	// Build a frequency diagram of words
+
+	map<string, int> WordCount;
+	for (auto word : Words)
+		WordCount[word]++;
+
+	// Find duplicate words
+	for (auto [word, count] : WordCount)
+	{
+		if (count > 1)
+			cout << word << " has " << count << " duplicates" << endl;
+	}
+
+	// Which one is the word with most duplicates
+	int MaxCount = 0;
+	string MaxCountWord = "";
+	for (auto [word, count] : WordCount)
+	{
+		if (count > MaxCount)
+		{
+			MaxCount = count;
+			MaxCountWord = word;
+		}
+	}
+	cout << MaxCountWord << " has most duplicates " << MaxCount << endl;
+
 	return 0;
 }
