6fe31ad8fae544f899f8a7d1528648a49e085788
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-05-18 15:40:34 +0300

    Heap array

--- Source.cpp
+++ Source.cpp
@@ -14,6 +14,21 @@ int main()
 	cin >> nIntegers;
 
 	// The following does not work since nIntegers is not a const.
-	int Array[nIntegers];
+	// int Array[nIntegers];
+
+	int* HeapArray = new int[nIntegers];
+	for (int i = 0; i < nIntegers; ++i)
+	{
+		cout << "Please enter item at index " << i << ": " << endl;
+		cin >> HeapArray[i];
+	}
+
+	int Total = 0;
+	for (int i = 0; i < nIntegers; ++i)
+		Total += HeapArray[i];
+	cout << "Total = " << Total << endl;
+
+	delete[] HeapArray;
+	HeapArray = nullptr;
 
 }
