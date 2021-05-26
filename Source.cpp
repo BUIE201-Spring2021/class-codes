233e8f13d19183f892e166e52aeaf17003102111
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-05-26 13:42:15 +0300

    Polymorphism

--- Source.cpp
+++ Source.cpp
@@ -35,6 +35,13 @@ void Dog::Communicate()
 	cout << "Hav" << endl;
 }
 
+// Polymorphism
+void CommunicateAll(vector<Animal*>& Animals)
+{
+	for (Animal* pAnimal : Animals)
+		pAnimal->Communicate();
+}
+
 
 int main()
 {
@@ -47,6 +54,29 @@ int main()
 	// Cannot create an instance of abstract class
 	// Animal a;
 	// a.Communicate();
+
+	vector<Animal*> Animals;
+	//Animals.push_back(new Dog);
+	//Animals.push_back(new Cat);
+	//Animals.push_back(new Dog);
+	//Animals.push_back(new Dog);
+
+	cout << "How many animals do you want to create?" << endl;
+	int nAnimals;
+	cin >> nAnimals;
+
+	for (int i = 0; i < nAnimals; ++i)
+	{
+		cout << "Please type C for cat and D for dog" << endl;
+		char type;
+		cin >> type;
+		if (type == 'C')
+			Animals.push_back(new Cat);
+		else
+			Animals.push_back(new Dog);
+	}
+
+	CommunicateAll(Animals);
 
 	return 0;
 }
