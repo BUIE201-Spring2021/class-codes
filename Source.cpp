0d3f6eb718f763364ba97973d89474d3add00fd1
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-05-26 13:21:30 +0300

    Abstract class

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// abstract class
+class Animal
+{
+
+public:
+	virtual void Communicate() = 0;	// pure virtual function
+};
+
+class Cat : public Animal
+{
+
+public:
+	virtual void Communicate();
+};
+
+void Cat::Communicate()
+{
+	cout << "Miyav" << endl;
+}
+
+class Dog : public Animal
+{
+
+public:
+	virtual void Communicate();
+};
+
+void Dog::Communicate()
+{
+	cout << "Hav" << endl;
+}
+
+
 int main()
 {
+	Dog d;
+	d.Communicate();
+
+	Cat c;
+	c.Communicate();
+
+	// Cannot create an instance of abstract class
+	// Animal a;
+	// a.Communicate();
 
 	return 0;
 }
