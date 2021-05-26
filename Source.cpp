7c1ef0c9564f008f57acc17a1a09cd58f4465aca
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-05-26 13:05:18 +0300

    Initial commit

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,9 @@
 #include <iostream>
-#include "Rectangle.h"
-#include "Square.h"
-#include "NewSquare.h"
 
 using namespace std;
 
 int main()
 {
-	//Rectangle r(5, 7);
-	//r.Print();
-
-	//Square s(4);
-	//s.Print();
-
-	//{
-	//	NewSquare ns(3);
-	//	ns.Print();
-	//}
-
-	NewSquare* pNS = new NewSquare(4);
-	pNS->Print();
-	pNS->VirtualPrint();
-	delete pNS;
-
-	Rectangle* pNS2 = new NewSquare(6);
-	pNS2->Print();
-	pNS2->VirtualPrint();
-	delete pNS2;
-
 
 	return 0;
 }
