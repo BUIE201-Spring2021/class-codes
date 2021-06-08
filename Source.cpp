4cdefbb6a2f0b3c9f7080a2b951f768a39a7738e
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-06-08 15:33:18 +0300

    Initial commit

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-#include "MyString.h"
 
 using namespace std;
 
 int main()
 {
-	MyString str("IE 201 Intermediate Programming");
-
-	bool Start1 = str.StartsWith("IE 201");
-
-	bool Start2 = str.StartsWith("Inter");
-
-	bool Contain1 = str.Contains("201");
-
-	bool Contain2 = str.Contains("305");
 
 }
