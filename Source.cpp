c2a6f53b94adbcd411b6d13937690ddf7906f812
Z. Caner Taskin <caner.taskin@boun.edu.tr>
2021-06-16 16:02:57 +0300

    Sort

--- Source.cpp
+++ Source.cpp
@@ -1,84 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-class TreeNode
-{
-public:
-	int Value;
-	TreeNode* pLeft;
-	TreeNode* pRight;
-
-	TreeNode(int ValueIn)
-	{
-		Value = ValueIn;
-		pLeft = nullptr;
-		pRight = nullptr;
-	}
-};
-
-// O(log n)
-void Insert(TreeNode*& pRoot, int value)
-{
-	if (pRoot == nullptr)
-		pRoot = new TreeNode(value);
-	else if (value > pRoot->Value)
-		Insert(pRoot->pRight, value);
-	else
-		Insert(pRoot->pLeft, value);
-}
-
-// O(log n)
-bool Find(TreeNode* pRoot, int value)
-{
-	if (pRoot == nullptr)
-		return false;
-	else if (value == pRoot->Value)
-		return true;
-	else if (value > pRoot->Value)
-		return Find(pRoot->pRight, value);
-	else
-		return Find(pRoot->pLeft, value);
-}
-
-// O(n)
-int Size(TreeNode* pRoot)
-{
-	if (pRoot == nullptr)
-		return 0;
-	else
-	{
-		int LeftSize = Size(pRoot->pLeft);
-		int RightSize = Size(pRoot->pRight);
-		return 1 + LeftSize + RightSize;
-	}
-}
-
-// In-order printing
-void Print(TreeNode* pRoot)
-{
-	if (pRoot)
-	{
-		Print(pRoot->pLeft);
-		cout << pRoot->Value << endl;
-		Print(pRoot->pRight);
-	}
-}
-
-
 int main()
 {
-	TreeNode* pRoot = nullptr;
-	Insert(pRoot, 15);
-	Insert(pRoot, 20);
-	Insert(pRoot, 6);
-	Insert(pRoot, 10);
-	Insert(pRoot, 3);
+	vector<int> myVector{ 4, -3, 19, 3, 5 };
 
-	bool Find6 = Find(pRoot, 6);
-	bool Find8 = Find(pRoot, 8);
+	// Simple sort algorithms like bubble sort, insertion sort: O(n^2)
+	// Quick sort, merge sort: O(n log n)
 
-	int size = Size(pRoot);
+	// STL: Standard Template Library
 
-	Print(pRoot);
+	sort(myVector.begin(), myVector.end());
+
+	for (int i : myVector)
+		cout << i << " ";
 }
